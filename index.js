71506807bef5af6e38dd3b1f2d62a774a2af3b73
Basiratkehinde <akinokunbasirat@gmail.com>
2023-08-01 15:42:53 +0100

    My Calculator

---
+++ index.js
@@ -0,0 +1,53 @@
+function show1() {
+document.getElementById("work").value+="4";
+}
+function show2() {
+    document.getElementById("work").value+="5";
+    }
+    function show3() {
+        document.getElementById("work").value+="6";
+    }
+    
+    function show4() {
+        document.getElementById("work").value+="/";
+    }
+
+ function show5() {
+        document.getElementById("work").value+="7";
+    }
+    function show6() {
+        document.getElementById("work").value+="8";
+    }
+    function show7() {
+        document.getElementById("work").value+="9";
+    }
+  
+    function tel1() {
+        document.getElementById("work").value+="1";
+    }
+    function tel2() {
+        document.getElementById("work").value+="2";
+    }
+    function tel3() {
+        document.getElementById("work").value+="3";
+    }
+    function tel4() {
+        document.getElementById("work").value+="*";
+    }
+    function tel5() {
+        document.getElementById("work").value+=".";
+    }
+    function tel6() {
+        document.getElementById("work").value+="0";
+    }
+    function tel7() {
+        document.getElementById("work").value+="+";
+    }
+    function exe() {
+        document.getElementById("work").value=eval(document.getElementById("work").value);
+    }
+    function show8() {
+        document.getElementById("work").value=document.getElementById("work").value.substr(0,document.getElementById("work").value.length-1)
+    }
+
+
